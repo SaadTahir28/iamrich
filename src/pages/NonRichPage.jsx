f3b68ccc1109c2d442b001636b6c15b59c65b172
Saad Tahir <saadtahir28@gmail.com>
2022-02-16 13:20:37 +0500

    Added public function call

--- src/pages/NonRichPage.jsx
+++ src/pages/NonRichPage.jsx
@@ -6,13 +6,17 @@ import gemAnimation from '../assets/animations/gem-animation.json';
 import Page from '../components/layout/Page';
 import { useEffect, useState } from 'react';
 import Buttons from '../assets/styles/components/buttons';
-import useUser from '../data/hooks/useUser';
-import { callReadOnlyFunction, cvToValue, cvToString } from "@stacks/transactions";
-import { AppConfig, showConnect, UserSession } from '@stacks/connect-react';
+import { 
+	callReadOnlyFunction, 
+	cvToValue, 
+	PostConditionMode, 
+	FungibleConditionCode, 
+	makeStandardSTXPostCondition,
+	uintCV } from "@stacks/transactions";
+import { AppConfig, showConnect, UserSession, openContractCall } from '@stacks/connect-react';
 import { StacksTestnet } from "@stacks/network";
 import * as constants from "../util/constants";
-
-const BigNum = require("bn.js");
+import BN from "bn.js";
 
 const network = new StacksTestnet();
 const appConfig = new AppConfig(['store_write', 'publish_data']);
@@ -23,9 +27,26 @@ function ConvertAmountInStacks(microStacks) {
 }
 
 export default function NonRichPage() {
-	const { user, isLoggedIn, logout } = useUser();
+	const [userData, setUserData] = useState(null)
 	const [richestPerson, setRichestPerson] = useState(null);
 	const [richestPersonAmount, setRichestPersonAmount] = useState(0);
+	const [nextRichestPersonAmount, setNextRichestPersonAmount] = useState(0);
+	const [amountCommission, setAmountCommission] = useState(0);
+
+	useEffect(() => {
+		if (userSession.isSignInPending()) {
+		  userSession.handlePendingSignIn().then((userData) => {
+			console.log("handlePendingSignIn: " + userData);
+			setUserData(userData);
+		  });
+		} else if (userSession.isUserSignedIn()) {
+		  console.log("already signed in");
+		  const userData = userSession.loadUserData();
+		  setUserData(userData);
+		} else {
+		  console.log("signed out");
+		}
+	  }, []);
 
 	useEffect(() => {
 		async function handleSubmit() {
@@ -61,8 +82,7 @@ export default function NonRichPage() {
 			try {
 				const result = await callReadOnlyFunction(options);
 				const response = cvToValue(result);
-				const amountInStacks = ConvertAmountInStacks(Number(response));
-				setRichestPersonAmount(amountInStacks);
+				setRichestPersonAmount(Number(response));
 				console.log("RPA", richestPersonAmount)
 			} catch (err) {
 				console.log(err);
@@ -71,41 +91,112 @@ export default function NonRichPage() {
 		handleSubmit();
 	}, [richestPersonAmount]);
 
+	useEffect(() => {
+		async function handleSubmit() {
+			const options = {
+				contractAddress: constants.contractAddress,
+				contractName: constants.contractName,
+				functionName: constants.getNextTransactionAmount,
+				functionArgs: [],
+				network,
+				senderAddress: constants.contractAddress,
+			};
+			try {
+				const result = await callReadOnlyFunction(options);
+				const response = cvToValue(result);
+				setNextRichestPersonAmount(Number(response));
+			} catch (err) {
+				console.log(err);
+			}
+		}
+		handleSubmit();
+	}, [nextRichestPersonAmount]);
+
+	useEffect(() => {
+		async function handleSubmit() {
+			const options = {
+				contractAddress: constants.contractAddress,
+				contractName: constants.contractName,
+				functionName: constants.getAmountCommission,
+				functionArgs: [uintCV(nextRichestPersonAmount)],
+				network,
+				senderAddress: constants.contractAddress,
+			};
+			try {
+				const result = await callReadOnlyFunction(options);
+				const response = cvToValue(result);
+				setAmountCommission(response);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+		handleSubmit();
+	}, [amountCommission]);
+
 	const onConnectClick = () => {
 		showConnect({
 			appDetails: {
-				name: 'iamrich',
-				icon: window.location.origin + '/my-app-logo.svg'
+				name: constants.appName,
+				icon: window.location.origin + constants.logo
 			},
 			redirectTo: '/',
 			onFinish: () => {
-				let userData = userSession.loadUserData();
 				// Save or otherwise utilize userData post-authentication
-				console.log(userData);
+				setUserData(userSession.loadUserData())
 			},
 			userSession: userSession
 		});
 	};
-	const onLogoutClick = () => logout();
-	const onRichClick = () => console.log('Handle api call here');
+	const onLogoutClick = () => userSession.signUserOut("/")
+
+	const onRichClick = async () => {
+		console.log('Handle api call here');
+		const stxPostCondition = makeStandardSTXPostCondition(
+			userData?.profile.stxAddress.testnet,
+			FungibleConditionCode.Equal,
+			new BN(amountCommission)
+		  );
+		const options = {
+			contractAddress: constants.contractAddress,
+			contractName: constants.contractName,
+			functionName: constants.becomeRichest,
+			functionArgs: [],
+			appDetails: {
+			  name: constants.appName,
+			  icon: window.location.origin + constants.logo,
+			},
+			network,
+			userSession,
+			postConditions: [stxPostCondition],
+			postConditionCode: PostConditionMode.Deny,
+			onFinish: (data) => {
+			  console.log("Stacks Transaction:", data.stacksTransaction);
+			  console.log("Transaction ID:", data.txId);
+			  console.log("Raw transaction:", data.txRaw);
+			},
+		  };
+		  await openContractCall(options);
+	}
 	
 	return (
 		<Page>
 			<PageSection minH={StyleVariables.NavbarHeight} justifyContent='flex-end'>
-				<Button onClick={isLoggedIn ? onLogoutClick : onConnectClick} size='md'>
-					{isLoggedIn ? user.id : 'Connect'}
+				<Button onClick={userSession.isUserSignedIn() ? onLogoutClick : onConnectClick} size='md'>
+					{userSession.isUserSignedIn() ? userData?.profile.stxAddress.testnet : 'Connect'}
 				</Button>
 			</PageSection>
 			<PageSection alignItems='center' flexDirection='column'>
 				<LottieAnimation data={gemAnimation} />
 				<Text fontSize='l' textAlign='center'>Want to show your friends your are rich?</Text>
 				<Text fontSize='2xl' mt='2' textAlign='center'>
-					Current richest person is <i><b>{richestPerson}</b></i> with <i><b>{richestPersonAmount} STX</b></i>
+					Current richest person is <i><b>{richestPerson}</b></i> with <i><b>{ConvertAmountInStacks(richestPersonAmount)} STX</b></i>
 				</Text>
-				<Button onClick={onRichClick} size='lg' mt='8' {...Buttons.variants.red} disabled={!isLoggedIn}>
+				<Text fontSize='2xl' mt='2' textAlign='center'>
+					You can become the next richest person with <i><b>{ConvertAmountInStacks(nextRichestPersonAmount)} STX</b></i>
+				</Text>
+				<Button onClick={onRichClick} size='lg' mt='8' {...Buttons.variants.red} disabled={!userSession.isUserSignedIn()}>
 					I am rich
 				</Button>
-
 			</PageSection>
 		</Page>
 	);
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,5 +1,11 @@
+export const appName = "RichiRich"
+export const logo = '/my-app-logo.svg'
+
 export const contractAddress = "ST5GACDNGCT91KQYMHFQ6BDDFS034RQK5FX4JSWW"
 export const contractName = "richirich"
 
 export const getLastTransactionAmount = "get-last-transaction-amount"
 export const getCurrentRichest = "get-current-richest"
+export const getNextTransactionAmount = "get-next-transaction-amount"
+export const becomeRichest = "become-richest"
+export const getAmountCommission = "get-amount-commission"
